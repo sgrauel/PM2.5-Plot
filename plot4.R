3079a5268d5eaed67a63a0621589884365acc469
sgrauel <shawn.m.grauel@gmail.com>
2015-07-26 19:13:53 -0400

    plot 4; Fuel Combustion Related Sources

---
+++ plot4.R
@@ -0,0 +1,25 @@
+# plot for question 4
+
+makePlot4 <- function() {
+  
+  NEI <- readRDS("summarySCC_PM25.rds")
+  SCC <- readRDS("Source_Classification_Code.rds")
+  
+  
+  # merge two data frames by foreign key 'SCC', unique identifier for the source
+  # **WARNING*** : expensive operation
+  mergedNEIandSCC <- merge(NEI,SCC, by.x = "SCC", by.y = "SCC")
+  
+  library(dplyr)
+  
+  # filter for all observations who are coal-combusion related sources
+  mergedNEIandSCC <- mergedNEIandSCC %>% filter(grepl(glob2rx("Fuel Comb - * - Coal"),EI.Sector))
+  
+  library(ggplot2)
+  
+  plt <- qplot(year, Emissions, data = mergedNEIandSCC,
+               geom="bar", stat = "identity",
+               ylab = expression("PM"[2.5]*" (Tons)"), xlab = "Years", colour = EI.Sector, main = expression("US PM"[2.5]*" Emissions from Coal Combustion Related Sources (1999 - 2008)")) + theme_bw(base_family = "merriweather", base_size = 12)
+  
+  ggsave(filename = "./plot4.png")
+}
